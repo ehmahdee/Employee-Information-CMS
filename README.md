fe2747a00fec6159b0678cd5a03a691e987dc04b
Fancy Pants <edaily94@gmail.com>
2023-04-13 17:38:46 -0700

    added view options for the root questions

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Employee-Information-CMS
+
+  ## Description
+  
+  This is a helpful command line app that simplifies and expedites README writing.
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Screenshots](#screenshots)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  This app requires nodejs, node inquirer 8.2.4, and node jest.
+
+  ## Usage
+
+  The app is used through the command terminal. after running the app, you will be prompted to answer inputs through the terminal that will then populate in the generated README.
+
+  ## License
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+  
+  This project is licensed under the MIT license.
+
+   ## Screenshots
+  [screencastify]()
+
+  ## Contributions and Credits
+
+  My discord study group: Michael Huang, Michael Rader, and Patricia Alberto.
+
+  ## Test Commands
+
+  Run this app using node index.js command in the terminal.
+
+  ## Questions
+
+  If you have any questions, please email me at edaily94@gmail.com. Visit my [GitHub profile](https://github.com/ehmahdee) to see my other projects.
--- server.js
+++ server.js
@@ -1,180 +1,258 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // const department = require('./');
 // const employee = require('./');
 // const role = require('./');
 
-
-
 const beginQuestions = () => {
-    inquirer.prompt([
-    {
-    type: 'list',
-    name: 'menu',
-    message: 'What would you like to do?',
-    choices: [
-        'View all departments',
-        'Add/update departments',
-        'View all roles',
-        'add/update roles',
-        'View all employees',
-        'Add/update employees',
-        'Additional reports',
-        'Exit'
-    ]
-}
-])
-.then((answer) => {
-    switch (answer.menu) {
-        case 'View all departments':
-            departments();
-            break;
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "Add/update departments",
+          "View all roles",
+          "add/update roles",
+          "View all employees",
+          "Add/update employees",
+          "Additional reports",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.menu) {
+        case "View all departments":
+          viewDepartments();
+          break;
 
-        case 'Add/update departments':
-            addDepartment();
-            break;
+        case "Add/update departments":
+          addDepartment();
+          break;
 
-        case 'View all roles':
-            roles();
-            break;
+        case "View all roles":
+          viewRoles();
+          break;
 
-        case 'add/update roles':
-            addRole();
-            break;
+        case "add/update roles":
+          addRole();
+          break;
 
-        case 'View all employees':
-            employees();
-            break;
+        case "View all employees":
+          viewEmployees();
+          break;
 
-        case 'Add/update employees':
-            addEmployee();
-            break;
+        case "Add/update employees":
+          addEmployee();
+          break;
 
-        case 'Additional reports':
-            additionalReports();
-            break;
+        case "Additional reports":
+          additionalReports();
+          break;
 
         default:
-            console.log('See you later!');
-            process.exit();
-            break;
-    }
+          console.log("See you later!");
+          process.exit();
+          break;
+      }
+    });
+};
 
-})
-}
-
-const addDepartment = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'addDepartment',
-            message: 'Choose one of the options below:',
-            choices: [
-                'Add department',
-                'Edit departments',
-                'Delete department',
-                'Go back' ]
-        }
+const viewDepartments = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "viewDepartments",
+        message: "Choose one of the options below:",
+        choices: [
+                "View all departments",  
+                "Go Back"
+            ],
+      },
     ])
-  .then((answer) => {
-    switch (answer.addDepartment) {
-        case 'Add department':
-            console.log('\n');
-            addDepartment();
-            break;
-
-            case 'Edit departments':
-                console.log('\n');
-                editDepartment();
-                break;
-
-            case 'Delete department':
-                console.log('\n');
-                deleteDepartment();
+    .then((answer) => {
+        switch (answer.viewDepartments) {
+            case "View all departments":
+                console.log("\n");
+                viewDepartments();
                 break;
 
             default:
-                console.log('\n')
+                console.log("\n");
                 menu();
         }
-    })
-}
+    });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "addDepartment",
+        message: "Choose one of the options below:",
+        choices: [
+          "Add department",
+          "Edit departments",
+          "Delete department",
+          "Go back",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.addDepartment) {
+        case "Add department":
+          console.log("\n");
+          addDepartment();
+          break;
+
+        case "Edit departments":
+          console.log("\n");
+          editDepartment();
+          break;
+
+        case "Delete department":
+          console.log("\n");
+          deleteDepartment();
+          break;
+
+        default:
+          console.log("\n");
+          menu();
+      }
+    });
+};
+
+const viewRoles = () => {
+    inquirer
+    .prompt([
+        {
+          type: "list",
+          name: "viewRoles",
+          message: "Choose one of the options below:",
+          choices: [
+            "View all roles",
+            "Go back",
+          ],
+        },
+      ])
+    .then((answer) => {
+        switch (answer.viewRoles) {
+          case "View all roles":
+            console.log("\n");
+            viewRoles();
+            break;
+
+          default:
+            console.log("\n");
+            menu();
+        }
+      });
+  };
+
 
 const addRole = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "addRole",
+      message: "Choose one of the options below:",
+      choices: [
+        "Add role", 
+        "Edit roles", 
+        "Delete role", 
+        "Go back"
+    ],
+    }.then((answer) => {
+      switch (answer.addRole) {
+        case "Add role":
+          console.log("\n");
+          addRole();
+          break;
+
+        case "Edit roles":
+          console.log("\n");
+          editRole();
+          break;
+
+        case "Delete role":
+          console.log("\n");
+          deleteRole();
+          break;
+
+        default:
+          console.log("\n");
+          menu();
+      }
+    }),
+  ]);
+};
+
+const viewEmployees = () => {
     inquirer.prompt([
-        {
-            type: 'list',
-            name: 'addRole',
-            message: 'Choose one of the options below:',
-            choices: [
-                'Add role',
-                'Edit roles',
-                'Delete role',
-                'Go back'
-            ]    
-        }
-        .then((answer) => {
-            switch (answer.addRole) {
-                case 'Add role':
-                    console.log('\n');
-                    addRole();
-                    break;
+       {
+        type: "list",
+        name: "viewEmployees",
+        message: "Choose one of the options below:",
+        choices: [
+          "View all employees",
+          "Go back"
+        ],
+       }
+       .then((answer) => {
+        switch (answer.viewEmployees) {
+            case "View all employees":
+                console.log("\n");
+                viewEmployees();
+                break;
 
-                case 'Edit roles':
-                    console.log('\n');
-                    editRole();
-                    break;
-                
-                case 'Delete role':
-                    console.log('\n');
-                    deleteRole();
-                    break;
-
-                default:
-                    console.log('\n')
-                    menu();
+            default:
+                console.log("\n");
+                menu();
             }
-        })
+       })
     ])
 }
-
 
 const addEmployee = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'addEmployee',
-            message: 'Choose one of the options below:',
-            choices: [
-                'Add employee',
-                'Edit employees',
-                'Delete employee',
-                'Go back'
-            ]    
-        }
-       .then((answer) => {
-            switch (answer.addEmployee) {
-                case 'Add employee':
-                    console.log('\n');
-                    addEmployee();
-                    break;
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Choose one of the options below:",
+      choices: [
+        "Add employee", 
+        "Edit employees", 
+        "Delete employee", 
+        "Go back"
+    ],
+    }.then((answer) => {
+      switch (answer.addEmployee) {
+        case "Add employee":
+          console.log("\n");
+          addEmployee();
+          break;
 
-                case 'Edit employees':
-                    console.log('\n');
-                    editEmployee();
-                    break;
-                
-                case 'Delete employee':
-                    console.log('\n');
-                    deleteEmployee();
-                    break;
+        case "Edit employees":
+          console.log("\n");
+          editEmployee();
+          break;
 
-                default:
-                    console.log('\n')
-                    menu();
-            }
-        })
-    ])
-}
+        case "Delete employee":
+          console.log("\n");
+          deleteEmployee();
+          break;
+
+        default:
+          console.log("\n");
+          menu();
+      }
+    }),
+  ]);
+};
 
 beginQuestions();
